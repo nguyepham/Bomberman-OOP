44f524a026bd5a9735b776eb55d5b1e18ebe1ea2
Minh <phamquangminh1659@gmail.com>
2022-10-24 23:22:38 +0700

    Add simple menu.

--- src/main/java/game/bomman/MainApplication.java
+++ src/main/java/game/bomman/MainApplication.java
@@ -1,15 +1,22 @@
 package game.bomman;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws FileNotFoundException {
-        Game.init(stage);
-        Game.run();
+    public void start(Stage stage) throws IOException {
+        MenuController.stage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Bomberman");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
